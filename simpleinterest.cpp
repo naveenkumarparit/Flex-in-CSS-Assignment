25c926c63615fb23b1ed7065c6cb0236782905f3
naveenkumarparit <naveenkumarparit123@gmail.com>
2024-02-27 23:50:06 +0530

    first commit

---
+++ simpleinterest.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float si=0,p,t,r;
+cout<<"enter the value of p,t,r";
+cin>>p>>t>>r;
+si=p*t*r/100;
+cout<<"the value is"<<si;
+return 0;
+}
